4f8a125dbefa30e3b0848f934ac573b38ec94cc9
louis gueye <louis.gueye@gmail.com>
2015-11-20 13:10:40 +0100

    better organization

---
+++ 03_functions/countBeans.js
@@ -0,0 +1,13 @@
+function countBs(input) {
+  return countChar(input, "B");
+}
+
+function countChar(input, target) {
+  var result = 0;
+  for (var i = 0; i < input.length; i++) {
+    if (input.charAt(i) == target) {
+      result ++;
+    }
+  }
+  return result;
+}
