e6d8ca8ec3fc6aa830bd17cae7e21ce630ea32b9
louis gueye <louis.gueye@gmail.com>
2015-12-01 17:39:38 +0100

    Chapter 05 complete

---
+++ 05_higher_order/every_some.js
@@ -0,0 +1,23 @@
+function every(input, predicate) {
+  var result = true;
+  input.forEach(function(item) {
+    if (!predicate(item)) {result = false; return}
+  });
+  return result;
+}
+function some(input, predicate) {
+  var result = false;
+  input.forEach(function(item) {
+    if (predicate(item)) {result = true; return}
+  });
+  return result;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
